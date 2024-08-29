0962a5c52b940ddf4c7f396d12363a33925bb8f7
Franco Alvarez Muñoz <falvarez.munoz@hotmail.es>
2024-08-29 19:01:47 +0000

    function that calculate minutes and hours moved to switch on Timer.jsx

--- src/js/component/Timer.jsx
+++ src/js/component/Timer.jsx
@@ -1,6 +1,29 @@
 import React from "react";
 
-const Timer = ({ compLabel, id }) => {
+
+const Timer = ({ compLabel, id, seconds }) => {
+
+    switch (id) {
+        case "unitSeconds":
+            compLabel = seconds % 10;
+            break;
+        case "tensSeconds":
+            compLabel = Math.floor((seconds % 60) / 10);
+            break;
+        case "unitMinutes":
+            compLabel = (Math.floor(seconds / 60) % 10);
+            break;
+        case "tensMinutes":
+            compLabel = Math.floor((Math.floor(seconds / 60) % 60 / 10));
+            break;
+        case "unitHours":
+            compLabel = (Math.floor(seconds / 3600) % 10);
+            break;
+        case "tensHours":
+            compLabel = Math.floor((Math.floor(seconds / 3600) % 60 / 10));
+            break;
+    }
+
     return (<h1 id={id} className="time mx-1 text-center">{compLabel}</h1>);
 }
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -16,14 +16,14 @@ const Home = () => {
 	return (
 		<div className="d-flex justify-content-center align-items-center pt-1 bg-dark">
 			<i class="fa-regular fa-4x fa-clock text-white me-2"></i>
-			<Timer id="unitMinutes" compLabel={Math.floor((Math.floor(sec / 3600) % 60 / 10))} />
-			<Timer id="unitMinutes" compLabel={(Math.floor(sec / 3600) % 10)} />
+			<Timer id="tensHours" seconds={sec} />
+			<Timer id="unitHours" seconds={sec} />
 			<h1>:</h1>
-			<Timer id="unitMinutes" compLabel={Math.floor((Math.floor(sec / 60) % 60 / 10))} />
-			<Timer id="unitMinutes" compLabel={(Math.floor(sec / 60) % 10)} />
+			<Timer id="tensMinutes" seconds={sec} />
+			<Timer id="unitMinutes" seconds={sec} />
 			<h1>:</h1>
-			<Timer id="tensSeconds" compLabel={Math.floor((sec % 60) / 10)} />
-			<Timer id="unitSeconds" compLabel={sec % 10} />
+			<Timer id="tensSeconds" seconds={sec} />
+			<Timer id="unitSeconds" seconds={sec} />
 		</div>
 	);
 };
